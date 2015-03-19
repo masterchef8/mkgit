09d9759b1db40d55673c4dfdfd1e07cdcecb9f64
Gabriel Augendre <gabriel@augendre.info>
2015-03-19 11:54:24 +0100

    Create folder works like a charm.

--- mkgit.c/test.c
+++ mkgit.c/test.c
@@ -5,7 +5,24 @@
 #include "test.h"
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <stdio.h>
 
 int main(int argc, char** argv) {
+    int success_create = 0;
+
+    if (argc < 2 || argc > 3) {
+        perror("Usage : mkdir <directory> [\"<Readme message>\"]");
+        return 1;
+    }
+
+    success_create = mkdir(argv[1], 0775);
+    if (success_create == 0 && argc == 2)
+        return 0;
+
+    if (success_create != 0) {
+        perror("Error while creating folder.");
+        return -1;
+    }
+
     return 0;
 }
