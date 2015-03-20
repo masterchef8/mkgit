1c71c799c9e52d81ba754faf331cd88212bbfb98
Gabriel Augendre <gabriel@augendre.info>
2015-03-20 10:33:37 +0100

    Make the git initialization work.

--- mkgit.c/test.c
+++ mkgit.c/test.c
@@ -6,22 +6,61 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdio.h>
+#include <git2.h>
+#include <string.h>
+
+#define false 0
 
 int main(int argc, char** argv) {
     int success_create = 0;
+    git_repository *repo = NULL;
+    int error = 0;
+    FILE *readme = NULL;
 
     if (argc < 2 || argc > 3) {
         perror("Usage : mkdir <directory> [\"<Readme message>\"]");
-        return 1;
+        return 2;
     }
 
     success_create = mkdir(argv[1], 0775);
-    if (success_create == 0 && argc == 2)
-        return 0;
 
     if (success_create != 0) {
-        perror("Error while creating folder.");
-        return -1;
+        perror("Error while creating folder");
+        return 1;
+    }
+
+    error = git_repository_init(&repo, argv[1], false);
+
+    if (argc == 3) {
+        char *readme_path = malloc(sizeof(char) * 1024);
+        char *line = malloc(sizeof(char) * 1024);
+        strcpy(readme_path, argv[1]);
+        strcat(readme_path, "/README.md");
+        readme = fopen(readme_path, "w+");
+
+        if (readme == NULL) {
+            perror("Error while opening README file");
+            return 4;
+        }
+
+        strcpy(line, "# ");
+        strcat(line, argv[1]);
+        strcat(line, "\n");
+        error = fputs(line, readme);
+        if (error == EOF) {
+            perror("Error while writing README file");
+            return 3;
+        }
+
+        strcpy(line, argv[2]);
+        strcat(line, "\n");
+        error = fputs(line, readme);
+        if (error == EOF) {
+            perror("Error while writing README file");
+            return 3;
+        }
+
+        fclose(readme);
     }
 
     return 0;
